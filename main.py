a2bce42312e68f6d41e60bb502960e3d24327f98
vicky <vickycao25@gmail.com>
2025-02-13 13:15:19 -0500

    asdfasdf

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-print("hello")
+
 
 
 print("hi")
+
+
+
+
+
+
+
+
+
+
+emg = MuscleSensor (3)
+led_object = LED (5)
+servo = Servo (8)
+motor = (forward = 16, backward = 20 )
+
+while True:
+    emg_value = emg.muscle_raw()
+
+
+
+
