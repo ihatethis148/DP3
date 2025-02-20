3db427be183d95d740a95853cd8465d50f16f454
vicky <vickycao25@gmail.com>
2025-02-20 13:12:41 -0500

    ihh

--- main.py
+++ main.py
@@ -4,11 +4,26 @@ emg = MuscleSensor (3)
 led_object = LED (5)
 servo = Servo (8)
 motor = (forward = 16, backward = 20 )
+t_end = time.time() + 60
 
 while True:
+    servo.min (-1)
+    servo.max (1) #Replace with correct values
+    servo.mid (0)
+
+    led_object.on()
     emg_value = emg.muscle_raw()
 
-def dc_motor
+    if emg_value == 0: #Replace with resting value or when EMG light is off and not recieving data
+        while time.time() < t_end:
+        motor.backward() #Change argument to speed you want
+
+    elif emg_value >0 and emg_value < 40: #Replace values with correct range
+
+
+
+
+
 
 
 
